f292177758a27d448289e01d26dfbea3ba43db4f
daisuke-c <09p061@gmail.com>
2020-12-16 00:37:08 +0900

    課題2と3を終了

---
+++ kadai2/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let question = prompt('日本の首都は?');
+
+if (question === '東京') {
+  alert('正解です！')
+} else {
+  alert('不正解です！');
+}
---
+++ kadai3/main.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const tasklist = ['掃除', '買い物', '散歩'];
+
+function haveTask() {
+  console.log('========================');
+  console.log('現在持っているタスク一覧');
+  console.log('========================');
+}
+
+const taskCount = function () {
+  for (let i = 0; i < tasklist.length; i++) {
+    console.log(`${i} : ${tasklist[i]}`);
+  }
+}
+
+haveTask();
+taskCount();
+
+let question = prompt('タスクを入力してください');
+
+
+if (question !== null && question !== '' && question !== 'タスクを入力してください') {
+  alert('新しいタスクを追加しました');
+  tasklist.push(question);
+
+  haveTask();
+  taskCount();
+
+  prompt('｢確認、追加、終了｣の4ついずれかを入力してください');
+}
